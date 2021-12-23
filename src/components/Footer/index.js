dd2b4a44ef72df9fd32503bac5f9988e22d1bda9
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-12-23 14:23:59 -0300

    Adicionando icones na Li de contatos e criando Li com informações sobre  Newsletter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,23 @@ import {
   FooterWrapper,
   Ul,
   Link,
+  ContactLi,
+  OpeningTitle,
+  OpeningText,
+  OpeningLi,
+  NewsletterLi,
+  NewsletterText,
+  InputContainer,
+  Input,
+  Submit,
+  ContactIcons,
+  IconContainer,
+  Icon,
 } from "./style";
+
+import instagram_icon from "../../assets/instagram-icon.svg";
+import facebook_icon from "../../assets/facebook-icon.svg";
+import twitter_icon from "../../assets/twitter-icon.svg";
 
 export default function Footer() {
   return (
@@ -35,12 +51,56 @@ export default function Footer() {
         </Content>
         <Content>
           <CursiveText>Contact</CursiveText>
+          <Ul>
+            <ContactLi>
+              <Link to={"/"}>123 Street New York USA</Link>
+            </ContactLi>
+            <ContactLi>
+              <Link to={"/"}>+012 345 67890</Link>
+            </ContactLi>
+            <ContactLi>
+              <Link to={"/"}>info@example.com</Link>
+            </ContactLi>
+            <ContactIcons>
+              <IconContainer>
+                <Icon src={twitter_icon}></Icon>
+              </IconContainer>
+              <IconContainer>
+                <Icon src={facebook_icon}></Icon>
+              </IconContainer>
+              <IconContainer>
+                <Icon src={instagram_icon}></Icon>
+              </IconContainer>
+            </ContactIcons>
+          </Ul>
         </Content>
         <Content>
           <CursiveText>Opening</CursiveText>
+          <Ul>
+            <OpeningLi>
+              <OpeningTitle>Monday - Saturday</OpeningTitle>
+              <OpeningText>09am - 09pm</OpeningText>
+            </OpeningLi>
+
+            <OpeningLi>
+              <OpeningTitle>Sunday</OpeningTitle>
+              <OpeningText>10am - 08pm</OpeningText>
+            </OpeningLi>
+          </Ul>
         </Content>
         <Content>
           <CursiveText>Newsletter</CursiveText>
+          <Ul>
+            <NewsletterLi>
+              <NewsletterText>
+                Dolor amet sit justo amet elitr clita ipsum elitr est.
+              </NewsletterText>
+              <InputContainer>
+                <Input type="email" placeholder="Your email"></Input>
+                <Submit>signup</Submit>
+              </InputContainer>
+            </NewsletterLi>
+          </Ul>
         </Content>
       </Container>
     </FooterWrapper>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { Link as anchor } from "react-router-dom";
 
+import phone_icon from "../../assets/phone-icon.svg";
+import location_icon from "../../assets/location-icon.svg";
+import mail_icon from "../../assets/mail-icon.svg";
+
 export const FooterWrapper = styled.footer`
   background-color: #0f172b;
   padding: 2rem 0;
@@ -25,8 +29,6 @@ export const Ul = styled.ul`
   list-style-type: none;
 `;
 
-export const CompanyLi = styled.li``;
-
 export const Link = styled(anchor)`
   display: block;
   font-size: 1.25rem;
@@ -42,25 +44,149 @@ export const Link = styled(anchor)`
     font-size: 1.3rem;
 
     @media screen and (min-width: 800px) {
-      font-size: 1rem;
+      font-size: 1.1rem;
     }
   }
   &:nth-of-type(1) {
     margin-top: 1.25rem;
   }
 
-  &::before {
-    content: ">";
-    font-family: "Poppins", sans-serif;
-    font-size: 20px;
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translateY(-50%);
+  @media screen and (min-width: 800px) {
+    font-size: 1rem;
+  }
+`;
+
+export const CompanyLi = styled.li`
+  ${Link} {
+    &::before {
+      content: ">";
+      font-family: "Poppins", sans-serif;
+      font-size: 20px;
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+`;
+
+export const ContactLi = styled.li`
+  ${Link} {
+    padding-left: 25px;
+    &::before {
+      content: "";
+      width: 21px;
+      height: 21px;
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
   }
 
-  @media screen and (min-width: 800px) {
-    font-size: 0.9rem;
+  &:nth-of-type(1) {
+    ${Link} {
+      &::before {
+        background-image: url(${location_icon});
+      }
+    }
   }
+  &:nth-of-type(2) {
+    ${Link} {
+      &::before {
+        background-image: url(${phone_icon});
+      }
+    }
+  }
+  &:nth-of-type(3) {
+    ${Link} {
+      &::before {
+        background-image: url(${mail_icon});
+      }
+    }
+  }
+`;
+
+export const ContactIcons = styled.li`
+  display: flex;
+  gap: 10px;
+  margin: 1rem 0;
+`;
+
+export const IconContainer = styled.figure`
+  width: 32px;
+  height: 32px;
+  border-radius: 50px;
+  border: 1px solid #fff;
+  padding: 7px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Icon = styled.img`
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
+`;
+
+export const OpeningLi = styled.li`
+  display: block;
+  margin: 1.1rem 0;
+`;
+
+export const OpeningTitle = styled.h5`
+  color: #fff;
+  font-family: "Nunito", sans-serif;
+  font-size: 1.3rem;
+`;
+
+export const OpeningText = styled.p`
+  color: #fff;
+  font-size: 1.25rem;
+
+  @media screen and (min-width: 800px) {
+    font-size: 1rem;
+  }
+`;
+
+export const NewsletterLi = styled(OpeningLi)``;
+
+export const NewsletterText = styled(OpeningText)``;
+
+export const InputContainer = styled.div`
+  background-color: white;
+  position: relative;
+  margin: 1rem 0;
+`;
+
+export const Input = styled.input`
+  display: block;
+  font-size: 0.9rem;
+  color: #666565;
+  border: 0;
+  height: 50px;
+  width: 100%;
+  padding-left: 10px;
+`;
+
+export const Submit = styled.button`
+  padding: 8px 16px;
+  background-color: #fea116;
+  color: #fff;
+  font-family: "Nunito", sans-serif;
+  font-size: 1rem;
+  text-transform: uppercase;
+  border: 0;
+  cursor: pointer;
+  position: absolute;
+  right: 5px;
+  top: 50%;
+  transform: translateY(-50%);
 `;
